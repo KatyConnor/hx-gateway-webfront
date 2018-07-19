9d36d2a7b83f3c4759928ca71444e71bec1908af
andyliang <2825523288@qq.com>
2018-07-19 09:50:17 +0800

    网关配置

---
+++ src/test/java/hx/framework/gateway/webfront/AppTest.java
@@ -0,0 +1,20 @@
+package hx.framework.gateway.webfront;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
